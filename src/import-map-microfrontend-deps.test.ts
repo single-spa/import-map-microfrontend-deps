4029d02ea89adbddaf9260e290357f750048f4d5
Joel Denning <joeldenning@gmail.com>
2024-10-17 15:51:16 -0600

    Initial implementation

---
+++ src/import-map-microfrontend-deps.test.ts
@@ -0,0 +1,63 @@
+import { beforeEach, test, vi, expect } from "vitest";
+import { buildImportMapDependencies } from "./import-map-microfrontend-deps";
+import { ImportMapMicrofrontendUtils } from "@single-spa/import-map-microfrontend-utils";
+import { vol, fs } from "memfs";
+
+vi.mock("node:fs/promises");
+
+beforeEach(() => {
+  vol.reset();
+});
+
+test(`writes an import map and dependencies to the file system`, async () => {
+  await buildImportMapDependencies({
+    template: {
+      imports: {
+        react: "18.3.0",
+        "react-dom": "18.3.0",
+      },
+      scopes: {},
+    },
+    outputFolder: "dist",
+    utils: new ImportMapMicrofrontendUtils({
+      baseOrigin: "https://cdn.example.com",
+    }),
+  });
+
+  expect(
+    await fs.readFileSync("./dist/deps.importmap", "utf-8"),
+  ).toMatchSnapshot();
+
+  expect(
+    await fs.readdirSync("./dist/deps", { recursive: true }),
+  ).toMatchSnapshot();
+});
+
+test(`scoped microfrontend dependencies`, async () => {
+  await buildImportMapDependencies({
+    template: {
+      imports: {
+        react: "18.3.0",
+        "react-dom": "18.3.0",
+      },
+      scopes: {
+        "@org/navbar": {
+          react: "17.0.0",
+          "react-dom": "17.0.0",
+        },
+      },
+    },
+    outputFolder: "dist",
+    utils: new ImportMapMicrofrontendUtils({
+      baseOrigin: "https://cdn.example.com",
+    }),
+  });
+
+  expect(
+    await fs.readFileSync("./dist/deps.importmap", "utf-8"),
+  ).toMatchSnapshot();
+
+  expect(
+    await fs.readdirSync("./dist/deps", { recursive: true }),
+  ).toMatchSnapshot();
+});
